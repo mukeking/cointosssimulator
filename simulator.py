f48045bdc0adea04598efd6453072376330c9c4d
Renzo <renzowesterbeek@gmail.com>
2013-11-17 11:15:17 +0100

    Added comments

--- simulator.py
+++ simulator.py
@@ -9,14 +9,17 @@ times = int(times)
 
 while count < times:
     rand = random.randint(1,2)
+    # Checks if head or tail #
     if rand == 1:
         heads += 1
     else:
         tails += 1
     count += 1
 
+# Basic formula for calculating percentage #
 percentage = (float(heads) / count) * 100
 
+# Print statements #
 print "Heads = " + str(heads)
 print "Tails = " + str(tails)
 print "Percentage heads = " + str(percentage) + "%"
